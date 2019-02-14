eb749363ced2c1564c72ea04dc44266ea4c9a3d3
jack.s <my_email@exp.co>
2019-02-14 12:24:22 +0800

    add continue

--- scatters/run.py
+++ scatters/run.py
@@ -2,8 +2,16 @@ import matplotlib.pyplot as plt
 
 from rand_walk import RandWalk
 
-rw=RandWalk()
-rw.fill_walk()
 
-plt.scatter(rw.xvalues,rw.yvalues,s=5,edgecolor='none')
-plt.show()
+active=True
+while active:
+    
+    rw=RandWalk(10000)
+    rw.fill_walk()
+
+    plt.scatter(rw.xvalues,rw.yvalues,s=5,edgecolor='none')
+    plt.show()
+    message=input("Do you want to continue('q' to quit):")
+    if message=='q':
+        active=False
+
