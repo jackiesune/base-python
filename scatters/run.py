1f7f31850aa3578189a23c361ea8c2a66f827264
jack.s <my_email@exp.co>
2019-02-14 12:50:45 +0800

    use cmap

--- scatters/run.py
+++ scatters/run.py
@@ -8,10 +8,15 @@ while active:
     
     rw=RandWalk(10000)
     rw.fill_walk()
+
     #设置窗口大小
     x,y=tuple(input("please enter number to control the size of figure :").split())
     plt.figure(figsize=(int(x),int(y)))
-    plt.scatter(rw.xvalues,rw.yvalues,s=5,edgecolor='none')
+#    plt.scatter(rw.xvalues,rw.yvalues,s=5,edgecolor='none')
+
+    #使用cmap
+    num_points=list(range(rw.num_points))
+    plt.scatter(rw.xvalues,rw.yvalues,s=5,c=num_points,edgecolor='none',cmap=plt.cm.Blues)
 
     #标记起点和终点
     plt.scatter(0,0,s=15,c='red',edgecolor='none')
